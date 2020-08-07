dcd997d44b5a0e79cea594fa452eb12a3ea88764
Ng Chan Way <chanwayng@u.nus.edu>
2020-08-07 14:36:48 +0800

    Circleci project setup (#7)
    * Add .circleci/config.yml
    
    * docs: Add status badge
    
    * docs: Add status badge
    
    * docs: Rename repo
    
    * docs: Merge README with master
    
    * test: Add test
    
    * test: Config circleci
    
    * test: Add tests
    
    * test: Update test
    
    * test: Update circleci config
    
    * test: Update test

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
-# Frame Extractor
+# imgextract
+[![CircleCI](https://circleci.com/gh/FirmaTechnologies/framextract.svg?style=shield)](https://circleci.com/gh/FirmaTechnologies/framextract)
+
 A CLI library to extract frames from a video
 
 ## Installation
-PyPI
-```bash
+[PyPI](https://pypi.org/project/frame-extractor/)
+```zsh
 pip install frame-extractor
 ```
 
 ## Usage
 CLI
-```bash
+```zsh
 framextract <inputfile> -o <outputfolder> -f <framerate>
 ```
 
 ## Development
 MacOS
-```bash
+```zsh
 python -m venv venv
 source venv/bin/activate
-pip install wheel -r requirements.txt
-bumpversion [major][minor][patch]
-python setup.py sdist bdist_wheel
+pip install [wheel] -r requirements.txt
+bumpversion [--tag] [--commit] [major|minor|patch]
+python setup.py sdist [bdist_wheel]
 twine check dist/*
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 twine upload dist/*
+```
+
+## Tests
+```zsh
+pytest --durations=3 --cov-report=html --cov=framextract
 ```
---
+++ tests/test_cli.py
@@ -0,0 +1,41 @@
+import pytest, subprocess
+
+COMMAND = ['python', 'framextract']
+
+def capture(command):
+    proc = subprocess.run(command, capture_output=True)
+    return proc.stdout, proc.stderr, proc.returncode
+
+def test_framextract_no_param():
+    out, err, exitcode = capture(COMMAND)
+    assert exitcode != 0
+    assert out == b''
+    assert err.startswith(b'usage: framextract [-h] [--version]')
+
+def test_framextract():
+    command = COMMAND + ['tests/FT.mp4']
+    out, err, exitcode = capture(command)
+    assert exitcode == 0
+    assert out.endswith(b'104 frames were extracted to FT/\n')
+    assert err == b''
+
+def test_framextract_invalid_video():
+    command = COMMAND + ['FT.mp4']
+    out, err, exitcode = capture(command)
+    assert exitcode != 0
+    assert out.startswith(b'FT.mp4')
+    # assert b"OpenCV: Couldn't read video stream" in err
+
+def test_framextract_output():
+    command = COMMAND + ['tests/FT.mp4', '-o', 'tests/FT']
+    out, err, exitcode = capture(command)
+    assert exitcode == 0
+    assert out.endswith(b'frames were extracted to tests/FT/\n')
+    assert err == b''
+
+def test_framextract_framerate():
+    command = COMMAND + ['tests/FT.mp4', '-f', '4']
+    out, err, exitcode = capture(command)
+    assert exitcode == 0
+    assert out.endswith(b'2 frames were extracted to FT/\n')
+    assert err == b''
