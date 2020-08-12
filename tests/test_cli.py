8f705a7d5a826527a0cd605d853c7d12a566c082
Ng Chan Way <chanwayng@u.nus.edu>
2020-08-12 10:38:55 +0800

    feat: Constrain input frame rate (#12)
    * feat: Constrain input frame rate
    
    * chore: Update version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,43 +1,42 @@
 import pytest, subprocess, os
 from pathlib import Path
 
-COMMAND = ['python', 'framextract']
+COMMAND = ['python', 'framextract', 'tests/FT.mp4']
 
 def capture(command):
     proc = subprocess.run(command, capture_output=True)
     return proc.stdout, proc.stderr, proc.returncode
 
 def test_framextract_no_param():
-    out, err, exitcode = capture(COMMAND)
+    out, err, exitcode = capture(COMMAND[:-1])
     assert exitcode != 0
     assert err.startswith(b'usage: framextract [-h] [--version]')
 
 def test_framextract(tmp_path):
     pwd = Path().absolute()
-    command = ['python', pwd/'framextract', pwd/'tests/FT.mp4']
+    command = [COMMAND[0], pwd/COMMAND[1], pwd/COMMAND[-1]]
     os.chdir(tmp_path)
     out, err, exitcode = capture(command)
     os.chdir(pwd)
     assert exitcode == 0
     assert out.endswith(b'104 frames were extracted to FT/\n')
 
-def test_framextract_output(tmp_path):
-    command = COMMAND + ['tests/FT.mp4', '-o', tmp_path/'FT']
-    out, _, exitcode = capture(command)
-    assert exitcode == 0
-    assert out.endswith(f'frames were extracted to {tmp_path}/FT/\n'
-                        .encode('utf-8'))
-
-def test_framextract_framerate(tmp_path):
-    command = COMMAND + ['tests/FT.mp4', '-f', '4',
-                         '-o', tmp_path/'FT']
+def test_framextract_output_framerate(tmp_path):
+    command = COMMAND + ['-o', tmp_path/'FT', '-f', '4']
     out, _, exitcode = capture(command)
     assert exitcode == 0
     assert out.endswith(f'2 frames were extracted to {tmp_path}/FT/\n'
                         .encode('utf-8'))
 
 def test_framextract_get_info(tmp_path):
-    command = COMMAND + ['tests/FT.mp4', '--get-info-only']
-    out, err, exitcode = capture(command)
+    command = COMMAND + ['--get-info-only']
+    out, _, exitcode = capture(command)
     assert exitcode == 0
     assert out.endswith(b'Frame size: 960 X 540\n')
+
+def test_framextract_small_framerate(tmp_path):
+    command = COMMAND + ['-o', tmp_path/'FT', '-f', '.01']
+    out, _, exitcode = capture(command)
+    assert exitcode == 0
+    assert out.endswith(f'104 frames were extracted to {tmp_path}/FT/\n'
+                        .encode('utf-8'))
