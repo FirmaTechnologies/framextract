edee467e2be0409972d911912bffd695b3ac8fa3
Ng Chan Way <chanwayng@u.nus.edu>
2020-08-12 11:23:27 +0800

    test: Add invalid input test (#13)
    * test: Add invalid input test
    
    * chore: Update version

--- framextract/__main__.py
+++ framextract/__main__.py
@@ -16,7 +16,7 @@ def get_frame(vidcap, frame_rate, count, frame_folder):
 
 def main():
     parser = ArgumentParser(description='Extract frames from a video')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.2.1')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.2.2')
     parser.add_argument('input', help='input video')
     parser.add_argument('--get-info-only', dest='getinfo', action='store_true',
                         help='print video frame rate and frame size')
@@ -30,13 +30,17 @@ def main():
     frame_folder = args.output if args.output else Path(video_name).stem
 
     vidcap = cv2.VideoCapture(video_name)
-    frame_rate_ori = 1/int(vidcap.get(cv2.CAP_PROP_FPS))
+    try:
+        frame_rate_ori = 1/int(vidcap.get(cv2.CAP_PROP_FPS))
+    except ZeroDivisionError as e:
+        print(f'Couldn\'t read video stream from file "{video_name}"')
+        return e
     frame_rate = args.framerate if (args.framerate and
                                     args.framerate>frame_rate_ori) else None
 
     if get_info:
         print(f'{video_name}:')
-        print(f'Frame rate: {frame_rate_ori:.3}')
+        print(f'Frame rate: {frame_rate_ori:.3} s')
         print(f'Frame size: {int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))}',
               f'X {int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))}')
         return
