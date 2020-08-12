e7f7ffe56cb98b8fc3f029c6d53e0f1c54b50522
ngchanway <chanwayng@u.nus.edu>
2020-08-12 10:23:18 +0800

    feat: Constrain input frame rate

--- framextract/__main__.py
+++ framextract/__main__.py
@@ -21,19 +21,22 @@ def main():
     parser.add_argument('--get-info-only', dest='getinfo', action='store_true',
                         help='print video frame rate and frame size')
     parser.add_argument('--output', '-o', help='output folder')
-    parser.add_argument('--framerate', '-f', type=float, help='frame rate')
+    parser.add_argument('--framerate', '-f', type=float,
+                        help='frame rate that is larger than the original')
     args = parser.parse_args()
 
     video_name = args.input
     get_info = args.getinfo
     frame_folder = args.output if args.output else Path(video_name).stem
-    frame_rate = args.framerate
 
     vidcap = cv2.VideoCapture(video_name)
+    frame_rate_ori = 1/int(vidcap.get(cv2.CAP_PROP_FPS))
+    frame_rate = args.framerate if (args.framerate and
+                                    args.framerate>frame_rate_ori) else None
 
     if get_info:
         print(f'{video_name}:')
-        print(f'Frame rate: {1/int(vidcap.get(cv2.CAP_PROP_FPS)):.3}')
+        print(f'Frame rate: {frame_rate_ori:.3}')
         print(f'Frame size: {int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))}',
               f'X {int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))}')
         return
