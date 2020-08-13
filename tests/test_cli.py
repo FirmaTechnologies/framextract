04211c6d5adf8d3a71b1f5c0b8ad979b70dd2819
Ng Chan Way <chanwayng@u.nus.edu>
2020-08-13 10:32:17 +0800

    style: Format with pathlib (#14)
    * style: Format with pathlib
    
    * test: Refactor tests with pathlib
    
    * chore: Update version
    
    * chore: Update version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,22 +8,22 @@ def capture(command):
     return proc.stdout, proc.stderr, proc.returncode
 
 def test_framextract_no_param():
-    out, err, exitcode = capture(COMMAND[:-1])
+    _, err, exitcode = capture(COMMAND[:-1])
     assert exitcode != 0
     assert err.startswith(b'usage: framextract [-h] [--version]')
 
 def test_framextract(tmp_path):
-    pwd = Path().absolute()
+    pwd = Path.cwd()
     command = [COMMAND[0], pwd/COMMAND[1], pwd/COMMAND[-1]]
     os.chdir(tmp_path)
     out, _, exitcode = capture(command)
     os.chdir(pwd)
     assert exitcode == 0
-    assert out.endswith(b'104 frames were extracted to FT/\n')
+    assert out.endswith(b'104 frames were extracted to "FT"\n')
 
 def test_framextract_invalid_input_video():
     command = COMMAND[:-1] + ['FT.mp4']
-    out, err, exitcode = capture(command)
+    out, _, exitcode = capture(command)
     assert exitcode == 0
     assert out.endswith(b'Couldn\'t read video stream from file "FT.mp4"\n')
 
@@ -31,7 +31,7 @@ def test_framextract_output_framerate(tmp_path):
     command = COMMAND + ['-o', tmp_path/'FT', '-f', '4']
     out, _, exitcode = capture(command)
     assert exitcode == 0
-    assert out.endswith(f'2 frames were extracted to {tmp_path}/FT/\n'
+    assert out.endswith(f'2 frames were extracted to "{tmp_path}/FT"\n'
                         .encode('utf-8'))
 
 def test_framextract_get_info(tmp_path):
@@ -44,5 +44,5 @@ def test_framextract_small_framerate(tmp_path):
     command = COMMAND + ['-o', tmp_path/'FT', '-f', '.01']
     out, _, exitcode = capture(command)
     assert exitcode == 0
-    assert out.endswith(f'104 frames were extracted to {tmp_path}/FT/\n'
+    assert out.endswith(f'104 frames were extracted to "{tmp_path}/FT"\n'
                         .encode('utf-8'))
